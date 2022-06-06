8f97cad0a237567041692ac6124e22b535775033
root <root@DESKTOP-VL2AUMN.localdomain>
2022-06-06 18:31:09 +0900

    Initial commit

---
+++ src/main/java/com/example/test004/Test004Application.java
@@ -0,0 +1,13 @@
+package com.example.test004;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test004Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test004Application.class, args);
+	}
+
+}
